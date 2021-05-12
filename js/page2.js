3db29719b3c5815045e4962643e1f1e961ec6b67
JuliaSt-Amand <82475226+JuliaSt-Amand@users.noreply.github.com>
2021-05-11 23:17:50 -0400

    Create page2.js

---
+++ js/page2.js
@@ -0,0 +1,11 @@
+function make_lavender(){
+	var div= document.getElementById('div1');
+	var style= "background-color:lavender";
+div.style=style;
+}
+
+function make_blue(){
+var div= document.getElementById('div1');
+var style= "background-color:Blue";
+div.style=style;
+}

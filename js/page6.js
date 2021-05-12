2f741758ec953f67ea853def48cc1956e049e5a0
JuliaSt-Amand <82475226+JuliaSt-Amand@users.noreply.github.com>
2021-05-11 23:43:47 -0400

    Update page6.js

--- js/page6.js
+++ js/page6.js
@@ -5,25 +5,4 @@ window.onload = () => {
 }
 
 
-#div1{
-  background-color: lavender;
-border-style:double;
-font-size: 32px;
-border-width: 5px;
-border-radius:3px;
-border-color:green;
-}
-
-
-
-
-'button1'{
-  background-color: lavender;
-border-style:double;
-border-width: 5px;
-border-radius:3px;
-border-color:green;
-}
-
-
 

4455ebf4882fecf982db8fa4b69cf332cbeb8d25
JuliaSt-Amand <82475226+JuliaSt-Amand@users.noreply.github.com>
2021-05-11 23:17:24 -0400

    Update page6.js

--- js/page6.js
+++ js/page6.js
@@ -27,14 +27,3 @@ border-color:green;
 
 
 
-function make_lavender(){
-	var div= document.getElementById('div1');
-	var style= "background-color:lavender";
-div.style=style;
-}
-
-function make_blue(){
-var div= document.getElementById('div1');
-var style= "background-color:Blue";
-div.style=style;
-}

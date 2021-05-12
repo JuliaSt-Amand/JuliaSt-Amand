f386c044b5dfcae0cfc6924c0736eb2083f88a0e
JuliaSt-Amand <82475226+JuliaSt-Amand@users.noreply.github.com>
2021-05-11 23:16:37 -0400

    Update page6.js

--- js/page6.js
+++ js/page6.js
@@ -24,3 +24,17 @@ border-width: 5px;
 border-radius:3px;
 border-color:green;
 }
+
+
+
+function make_lavender(){
+	var div= document.getElementById('div1');
+	var style= "background-color:lavender";
+div.style=style;
+}
+
+function make_blue(){
+var div= document.getElementById('div1');
+var style= "background-color:Blue";
+div.style=style;
+}

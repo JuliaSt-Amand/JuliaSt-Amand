e6179990daa49d81eda410f4a2b3d4798b008240
JuliaSt-Amand <82475226+JuliaSt-Amand@users.noreply.github.com>
2021-05-11 23:44:01 -0400

    Update about.js

--- js/about.js
+++ js/about.js
@@ -9,3 +9,25 @@ var div= document.getElementById('div1');
 var style= "background-color:Blue";
 div.style=style;
 }
+
+
+
+#div1{
+  background-color: lavender;
+border-style:double;
+font-size: 32px;
+border-width: 5px;
+border-radius:3px;
+border-color:green;
+}
+
+
+
+
+'button1'{
+  background-color: lavender;
+border-style:double;
+border-width: 5px;
+border-radius:3px;
+border-color:green;
+}
